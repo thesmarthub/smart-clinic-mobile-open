b4f9387c37d1fcaa2bba27e872270de7d39d3acc
Christy Peters XtyBells <ilobahxty@gmail.com>
2021-03-10 08:43:11 +0100

    add confirmation box before registration

--- src/app/pages/hospitals/hospitals.page.ts
+++ src/app/pages/hospitals/hospitals.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 import { LoadHospitals } from 'src/app/actions/events/hospital';
+import { Store } from 'src/app/engine/store';
 import { HospitalService } from 'src/app/services/hospital.service';
 
 @Component({
@@ -8,8 +10,10 @@ import { HospitalService } from 'src/app/services/hospital.service';
   styleUrls: ['./hospitals.page.scss'],
 })
 export class HospitalsPage implements OnInit {
+  storeCtrl = new Store()
 
-  constructor(public hService: HospitalService) { }
+  constructor(public hService: HospitalService, 
+  public atrCtrl: AlertController) { }
 
   ngOnInit() {
   }
@@ -17,5 +21,32 @@ export class HospitalsPage implements OnInit {
   ionViewDidEnter() {
     this.hService.triggerEvent(LoadHospitals)
   }
+
+  async showConfirmAlert(item) {
+    const alert = await this.atrCtrl.create({
+      cssClass: 'my-custom-class',
+      header: `Hi ${this.storeCtrl.user.fname}`,
+      message: `Your profile wasn't found in <strong>${item.name}</strong>. Do you want to register there? `,
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          
+          }
+        }, {
+          text: 'Register',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+      
 
 }
