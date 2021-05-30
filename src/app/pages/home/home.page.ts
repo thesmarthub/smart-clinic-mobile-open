b12abea80137fcf9a8076a7f8a9ff6b6c44398b3
Christy Peters XtyBells <ilobahxty@gmail.com>
2021-05-30 08:57:22 +0100

    complete home page

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -9,37 +9,37 @@ import { Store } from "src/app/engine/store";
 export class HomePage implements OnInit {
   cards = [
     {
-      title: "appointments",
+      title: "Appointments",
       icon: "fa fa-calendar",
       link: "/tabs/view-appointments",
       img: "/assets/004-calendar.png",
     },
     {
-      title: "doctors",
+      title: "Doctors",
       icon: "fa fa-user",
       link: "/tabs/doctors",
       img: "/assets/019-doctor.png",
     },
     {
-      title: "prescriptions",
+      title: "Prescriptions",
       icon: "fa fa-prescription",
       link: "/tabs/prescription",
       img: "/assets/001-drug.png",
     },
     {
-      title: "laboratory",
+      title: "Laboratory",
       icon: "fa fa-vials",
       link: "/tabs/lab",
       img: "/assets/003-research.png",
     },
     {
-      title: "scans",
+      title: "Scans",
       icon: "fa fa-x-ray",
       link: "/tabs/radiology",
       img: "/assets/ante_natal.png",
     },
     {
-      title: "hospitals",
+      title: "Hospitals",
       icon: "fa fa-building",
       link: "/tabs/hospitals",
       img: "/assets/012-hospital-1.png",
