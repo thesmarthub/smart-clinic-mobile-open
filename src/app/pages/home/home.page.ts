dc74dfd2e7ca712ae0e3f46be1b078003ceea616
Christy Peters XtyBells <ilobahxty@gmail.com>
2021-06-08 10:51:30 +0100

    complete home page design

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -13,36 +13,42 @@ export class HomePage implements OnInit {
       icon: "fa fa-calendar",
       link: "/tabs/view-appointments",
       img: "/assets/004-calendar.png",
+      bgColor: "appt"
     },
     {
-      title: "Doctors",
+      title: "Talk to Doctor",
       icon: "fa fa-user",
       link: "/tabs/doctors",
       img: "/assets/019-doctor.png",
+      bgColor: "doctors"
     },
     {
       title: "Prescriptions",
       icon: "fa fa-prescription",
       link: "/tabs/prescription",
       img: "/assets/001-drug.png",
+      bgColor: "doctors"
     },
     {
       title: "Laboratory",
       icon: "fa fa-vials",
       link: "/tabs/lab",
       img: "/assets/003-research.png",
+      bgColor: "appt"
     },
     {
       title: "Scans",
       icon: "fa fa-x-ray",
       link: "/tabs/radiology",
       img: "/assets/in_bed.png",
+      bgColor: "appt"
     },
     {
       title: "Hospitals",
       icon: "fa fa-building",
       link: "/tabs/hospitals",
       img: "/assets/012-hospital-1.png",
+      bgColor: "doctors"
     },
   ];
 
