b9567008f817468e8cc27b9493db6a7f529377e0
Christy Peters XtyBells <ilobahxty@gmail.com>
2021-06-22 14:03:36 +0100

    create health shop page

--- src/app/pages/health-shop/health-shop.page.ts
+++ src/app/pages/health-shop/health-shop.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common'
 
 @Component({
   selector: 'app-health-shop',
@@ -7,9 +8,93 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HealthShopPage implements OnInit {
 
-  constructor() { }
+  cards = [
+    {
+      title: "Urinalysis",
+      icon: "fa fa-calendar",
+      link: "/tabs/view-appointments",
+      img: "/assets/014-options.png",
+      bgColor: "appt",
+    },
+    {
+      title: "Panadol",
+      icon: "fa fa-user",
+      link: "/tabs/doctors",
+      img: "/assets/014-options.png",
+      bgColor: "doctors",
+    },
+    {
+      title: "Inhaler",
+      icon: "fa fa-prescription",
+      link: "/tabs/prescription",
+      img: "/assets/014-options.png",
+      bgColor: "doctors",
+    },
+    {
+      title: "Gloves",
+      icon: "fa fa-prescription",
+      link: "/tabs/prescription",
+      img: "/assets/014-options.png",
+      bgColor: "doctors",
+    },
+    {
+      title: "Hand Sanitizer",
+      icon: "fa fa-prescription",
+      link: "/tabs/prescription",
+      img: "/assets/014-options.png",
+      bgColor: "doctors",
+    },
+  ]
+
+  categories = [
+    {
+      title: "Drugs",
+      icon: "fa fa-calendar",
+      link: "/tabs/view-appointments",
+      img: "/assets/014-options.png",
+      bgColor: "appt",
+    },
+    {
+      title: "Lab Tests",
+      icon: "fa fa-user",
+      link: "/tabs/doctors",
+      img: "/assets/014-options.png",
+      bgColor: "doctors",
+    },
+    {
+      title: "Scans",
+      icon: "fa fa-calendar",
+      link: "/tabs/view-appointments",
+      img: "/assets/014-options.png",
+      bgColor: "appt",
+    },
+    {
+      title: "Others",
+      icon: "fa fa-user",
+      link: "/tabs/doctors",
+      img: "/assets/014-options.png",
+      bgColor: "doctors",
+    },
+    
+  ]
+
+  constructor(public location:Location) { }
 
   ngOnInit() {
+  }
+
+  slideOpts = {
+    initialSlide: 0,
+    speed: 100,
+    freeMode:false,
+    autoplay: {
+      delay: 5000,
+    },
+    
+  };
+
+  goBack(){
+    this.location.back()
   }
 
 }
